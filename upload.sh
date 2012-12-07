699e60f3e0fb4fa4fa75dc5c1ddda99230d822d8
flopp <fpigorsch@gmail.com>
2012-12-07 20:31:22 +0100

    Upload-Skript

---
+++ upload.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+. .server.data
+
+ncftpput -u $LOGIN -p $PASSWD $SERVER $BASE/ $(cat files.root)
+ncftpput -u $LOGIN -p $PASSWD $SERVER $BASE/img/ $(cat files.img)
+ncftpput -u $LOGIN -p $PASSWD $SERVER $BASE/js/ $(cat files.js)
+
