c93fba719da1783dfb69d764d890a35fe08691f8
flopp <fpigorsch@gmail.com>
2013-02-10 17:36:05 +0100

    Hinweis auf Blog

--- beta.php
+++ beta.php
@@ -308,6 +308,7 @@ echo "<body onload=\"initialize( $clat, $clon, $zoom, '$maptype', '$markers' )\"
   </div>
   <div class="modal-body">
       <h4>Hier muss alles neu geschrieben werden...</h4>
+      Bis das geschehen ist, kann man im <a href'="http://blog.flopp-caching.de/category/karte/">Blog</a> nützliche Informationen finden.
   </div>
   <div class="modal-footer">
     <button class="btn btn-primary" data-dismiss="modal" aria-hidden="true">Ok</button>
