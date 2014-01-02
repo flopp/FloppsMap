a1647ca0307b0bc778a44c53e00fd525a80e8e17
flopp <fpigorsch@gmail.com>
2014-01-02 15:31:10 +0100

    removed unused files

--- create-test.sh
+++
@@ -1,8 +0,0 @@
-
-S=$(date +%s)
-sed "s/TSTAMP/$S/g" index-template.php > index.php
-mv index.php test.php
-sed -i 's/map\.js/map-test.js/g' test.php
-sed -i 's/okapi\.js/okapi-test.js/g' test.php
-cp js/map.js js/map-test.js
-cp js/okapi.js js/okapi-test.js
--- upload-test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-. ~/.server.data
-
-ncftpput -u $LOGIN -p $PASSWD $SERVER $BASE/ test.php
-ncftpput -u $LOGIN -p $PASSWD $SERVER $BASE/js/ js/map-test.js
-ncftpput -u $LOGIN -p $PASSWD $SERVER $BASE/js/ js/okapi-test.js
-
